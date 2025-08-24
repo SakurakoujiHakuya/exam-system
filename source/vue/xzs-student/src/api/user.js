105ef2e0c5212c12ddc4b7da8dfad7acf4a893bf
SakurakoujiHakuya <53645627+SakurakoujiHakuya@users.noreply.github.com>
2025-08-24 20:09:21 +0800

    首页mock数据

--- source/vue/xzs-student/src/api/user.js
+++ source/vue/xzs-student/src/api/user.js
@@ -2,10 +2,37 @@ import { post } from '@/utils/request'
 
 export default {
   createUser: query => post('/api/student/user/edit', query),
-  getCurrentUser: () => post('/api/student/user/current'),
+  // mock getCurrentUser
+  getCurrentUser: () => Promise.resolve({
+    code: 1,
+    message: '成功',
+    response: {
+      id: 97,
+      userUuid: 'd2d29da2-dcb3-4013-b874-727626236f47',
+      userName: 'student',
+      realName: 'Test',
+      age: 18,
+      role: 1,
+      sex: 1,
+      birthDay: '2019-09-01 00:00:00',
+      phone: '158800882',
+      lastActiveTime: '',
+      createTime: '2019-09-07 18:55:02',
+      modifyTime: '2021-06-09 17:04:31',
+      status: 1,
+      userLevel: 1,
+      classes: '1班',
+      imagePath: ''
+    }
+  }),
   getUserEvent: () => post('/api/student/user/log'),
   update: query => post('/api/student/user/update', query),
   messagePageList: query => post('/api/student/user/message/page', query),
   read: id => post('/api/student/user/message/read/' + id),
-  getMessageCount: () => post('/api/student/user/message/unreadCount')
+  // mock getMessageCount
+  getMessageCount: () => Promise.resolve({
+    code: 1,
+    message: '成功',
+    response: 0 //未读消息数量
+  })
 }
