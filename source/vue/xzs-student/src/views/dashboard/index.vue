9c57096eaee108b4ea3011176c9170bed663ac32
SakurakoujiHakuya <53645627+SakurakoujiHakuya@users.noreply.github.com>
2025-08-24 19:54:42 +0800

    答题记录布局

--- source/vue/xzs-student/src/views/dashboard/index.vue
+++ source/vue/xzs-student/src/views/dashboard/index.vue
@@ -1,30 +1,14 @@
 <template>
   <div style="margin-top: 10px">
-    <el-row>
-      <el-carousel :interval="5000" arrow="always" type="card">
-        <el-carousel-item >
-          <img src="@/assets/carousel/1.png" class="carousel-img">
-        </el-carousel-item>
-        <el-carousel-item >
-          <img src="@/assets/carousel/2.png" class="carousel-img">
-        </el-carousel-item>
-        <el-carousel-item >
-          <img src="@/assets/carousel/3.png" class="carousel-img">
-        </el-carousel-item>
-        <el-carousel-item >
-          <img src="@/assets/carousel/4.png" class="carousel-img">
-        </el-carousel-item>
-      </el-carousel>
-    </el-row>
     <el-row class="app-item-contain">
       <h3 class="index-title-h3" style="border-left: solid 10px #3651d4;">任务中心</h3>
       <div style="padding-left: 15px">
-        <el-collapse  v-loading="taskLoading"  accordion v-if="taskList.length!==0">
+        <el-collapse v-loading="taskLoading" accordion v-if="taskList.length !== 0">
           <el-collapse-item :title="taskItem.title" :name="taskItem.id" :key="taskItem.id" v-for="taskItem in taskList">
             <table class="index-task-table">
               <tr v-for="paperItem in taskItem.paperItems" :key="paperItem.examPaperId">
                 <td class="index-task-table-paper">
-                  {{paperItem.examPaperName}}
+                  {{ paperItem.examPaperName }}
                 </td>
                 <td width="70px">
                   <el-tag :type="statusTagFormatter(paperItem.status)" v-if="paperItem.status !== null" size="mini">
@@ -32,14 +16,17 @@
                   </el-tag>
                 </td>
                 <td width="80px">
-                  <router-link target="_blank" :to="{path:'/do',query:{id:paperItem.examPaperId}}" v-if="paperItem.status === null">
-                    <el-button  type="text" size="small">开始答题</el-button>
+                  <router-link target="_blank" :to="{ path: '/do', query: { id: paperItem.examPaperId } }"
+                    v-if="paperItem.status === null">
+                    <el-button type="text" size="small">开始答题</el-button>
                   </router-link>
-                  <router-link target="_blank" :to="{path:'/edit',query:{id:paperItem.examPaperAnswerId}}" v-else-if="paperItem.status === 1">
-                    <el-button  type="text" size="small">批改试卷</el-button>
+                  <router-link target="_blank" :to="{ path: '/edit', query: { id: paperItem.examPaperAnswerId } }"
+                    v-else-if="paperItem.status === 1">
+                    <el-button type="text" size="small">批改试卷</el-button>
                   </router-link>
-                  <router-link target="_blank" :to="{path:'/read',query:{id:paperItem.examPaperAnswerId}}" v-else-if="paperItem.status === 2">
-                    <el-button  type="text" size="small">查看试卷</el-button>
+                  <router-link target="_blank" :to="{ path: '/read', query: { id: paperItem.examPaperAnswerId } }"
+                    v-else-if="paperItem.status === 2">
+                    <el-button type="text" size="small">查看试卷</el-button>
                   </router-link>
                 </td>
               </tr>
@@ -55,9 +42,9 @@
           <el-card :body-style="{ padding: '0px' }" v-loading="loading">
             <img src="@/assets/exam-paper/show1.png" class="image">
             <div style="padding: 14px;">
-              <span>{{item.name}}</span>
+              <span>{{ item.name }}</span>
               <div class="bottom clearfix">
-                <router-link target="_blank" :to="{path:'/do',query:{id:item.id}}">
+                <router-link target="_blank" :to="{ path: '/do', query: { id: item.id } }">
                   <el-button type="text" class="button">开始做题</el-button>
                 </router-link>
               </div>
@@ -73,14 +60,14 @@
           <el-card :body-style="{ padding: '0px' }" v-loading="loading">
             <img src="@/assets/exam-paper/show2.png" class="image">
             <div style="padding: 14px;">
-              <span>{{item.name}}</span>
+              <span>{{ item.name }}</span>
               <p class="index-limit-paper-time">
-                <span>{{item.startTime}}</span>
-                <br/>
-                <span>{{item.endTime}}</span>
+                <span>{{ item.startTime }}</span>
+                <br />
+                <span>{{ item.endTime }}</span>
               </p>
               <div class="bottom clearfix">
-                <router-link target="_blank" :to="{path:'/do',query:{id:item.id}}">
+                <router-link target="_blank" :to="{ path: '/do', query: { id: item.id } }">
                   <el-button type="text" class="button">开始做题</el-button>
                 </router-link>
               </div>
@@ -96,7 +83,7 @@
 import { mapState, mapGetters } from 'vuex'
 import indexApi from '@/api/dashboard'
 export default {
-  data () {
+  data() {
     return {
       fixedPaper: [],
       timeLimitPaper: [],
@@ -106,7 +93,7 @@ export default {
       taskList: []
     }
   },
-  created () {
+  created() {
     let _this = this
     this.loading = true
     indexApi.index().then(re => {
@@ -123,10 +110,10 @@ export default {
     })
   },
   methods: {
-    statusTagFormatter (status) {
+    statusTagFormatter(status) {
       return this.enumFormat(this.statusTag, status)
     },
-    statusTextFormatter (status) {
+    statusTextFormatter(status) {
       return this.enumFormat(this.statusEnum, status)
     }
   },
@@ -143,54 +130,54 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .index-title-h3 {
-    font-size: 22px;
-    font-weight: 400;
-    color: #1f2f3d;
-    border-left: solid 10px #2ce8b4;
-    padding-left: 10px;
-  }
+.index-title-h3 {
+  font-size: 22px;
+  font-weight: 400;
+  color: #1f2f3d;
+  border-left: solid 10px #2ce8b4;
+  padding-left: 10px;
+}
 
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-  }
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+}
 
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
 
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
 
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
 
-  .button {
-    padding: 0;
-    float: right;
-  }
+.button {
+  padding: 0;
+  float: right;
+}
 
-  .image {
-    width: 50%;
-    height: 80%;
-    display: block;
-    margin: 20px auto 10px auto;
-  }
+.image {
+  width: 50%;
+  height: 80%;
+  display: block;
+  margin: 20px auto 10px auto;
+}
 
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
 
-  .clearfix:after {
-    clear: both
-  }
+.clearfix:after {
+  clear: both
+}
 </style>
--- source/vue/xzs-student/src/views/record/index.vue
+++ source/vue/xzs-student/src/views/record/index.vue
@@ -1,58 +1,73 @@
 <template>
-  <div style="margin-top: 10px" class="app-contain">
-     <el-row :gutter="50">
-       <el-col :span="18">
-         <el-table v-loading="listLoading" :data="tableData" fit highlight-current-row style="width: 100%" @row-click="itemSelect">
-           <el-table-column prop="id" label="序号" width="90px"/>
-           <el-table-column prop="paperName" label="名称"  />
-           <el-table-column prop="subjectName" label="学科"  width="70" />
-           <el-table-column label="状态" prop="status" width="100px">
-             <template slot-scope="{row}">
-               <el-tag :type="statusTagFormatter(row.status)">
-                 {{ statusTextFormatter(row.status) }}
-               </el-tag>
-             </template>
-           </el-table-column>
-           <el-table-column prop="createTime" label="做题时间"  width="170" />
-           <el-table-column  align="right" width="70">
-             <template slot-scope="{row}">
-               <router-link target="_blank" :to="{path:'/edit',query:{id:row.id}}" v-if="row.status === 1 ">
-                 <el-button  type="text" size="small">批改</el-button>
-               </router-link>
-               <router-link target="_blank" :to="{path:'/read',query:{id:row.id}}" v-if="row.status === 2 ">
-                 <el-button  type="text" size="small">查看试卷</el-button>
-               </router-link>
-             </template>
-           </el-table-column>
-         </el-table>
-         <pagination v-show="total>0" :total="total" :background="false" :page.sync="queryParam.pageIndex" :limit.sync="queryParam.pageSize"
-                     @pagination="search" style="margin-top: 20px"/>
-       </el-col>
-       <el-col  :span="6" >
-         <el-card  class="record-answer-info">
-            <el-form label-width="50%" >
-              <el-form-item label="系统判分：">
-                <span>{{selectItem.systemScore}}</span>
-              </el-form-item>
-              <el-form-item label="最终得分：">
-                <span>{{selectItem.userScore}}</span>
-              </el-form-item>
-              <el-form-item label="试卷总分：">
-                <span>{{selectItem.paperScore}}</span>
-              </el-form-item>
-              <el-form-item label="正确题数：">
-                <span>{{selectItem.questionCorrect}}</span>
-              </el-form-item>
-              <el-form-item label="总题数：">
-                <span>{{selectItem.questionCount}}</span>
-              </el-form-item>
-              <el-form-item label="用时：">
-                <span>{{selectItem.doTime}}</span>
-              </el-form-item>
-            </el-form>
-         </el-card>
-       </el-col>
-     </el-row>
+  <div class="modern-record-page">
+    <div class="modern-column-layout">
+      <el-card class="modern-info-card" shadow="hover">
+        <div class="modern-info-header">
+          <el-avatar icon="el-icon-user" size="large" class="modern-info-avatar" />
+          <div class="modern-info-title">答题统计</div>
+        </div>
+        <el-divider></el-divider>
+        <div class="modern-info-list">
+          <div class="modern-info-item">
+            <span class="modern-info-label">系统判分</span>
+            <span class="modern-info-value">{{ selectItem.systemScore }}</span>
+          </div>
+          <div class="modern-info-item">
+            <span class="modern-info-label">最终得分</span>
+            <span class="modern-info-value highlight">{{ selectItem.userScore }}</span>
+          </div>
+          <div class="modern-info-item">
+            <span class="modern-info-label">试卷总分</span>
+            <span class="modern-info-value">{{ selectItem.paperScore }}</span>
+          </div>
+          <div class="modern-info-item">
+            <span class="modern-info-label">正确题数</span>
+            <span class="modern-info-value">{{ selectItem.questionCorrect }}</span>
+          </div>
+          <div class="modern-info-item">
+            <span class="modern-info-label">总题数</span>
+            <span class="modern-info-value">{{ selectItem.questionCount }}</span>
+          </div>
+          <div class="modern-info-item">
+            <span class="modern-info-label">用时</span>
+            <span class="modern-info-value">{{ selectItem.doTime }}</span>
+          </div>
+        </div>
+      </el-card>
+      <el-card shadow="hover" class="modern-table-card">
+        <div class="modern-table-title">
+          <el-avatar icon="el-icon-document" size="medium" class="modern-avatar" />
+          <span>我的答题记录</span>
+        </div>
+        <el-divider></el-divider>
+        <el-table v-loading="listLoading" :data="tableData" fit highlight-current-row border stripe
+          class="modern-table" @row-click="itemSelect">
+          <el-table-column prop="id" label="序号" width="80" align="center" />
+          <el-table-column prop="paperName" label="名称" min-width="120" />
+          <el-table-column prop="subjectName" label="学科" width="80" align="center" />
+          <el-table-column label="状态" prop="status" width="110" align="center">
+            <template slot-scope="{row}">
+              <el-tag :type="statusTagFormatter(row.status)" effect="plain">
+                {{ statusTextFormatter(row.status) }}
+              </el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column prop="createTime" label="做题时间" width="170" align="center" />
+          <el-table-column align="center" width="100">
+            <template slot-scope="{row}">
+              <router-link target="_blank" :to="{ path: '/edit', query: { id: row.id } }" v-if="row.status === 1">
+                <el-button type="primary" size="mini" round>批改</el-button>
+              </router-link>
+              <router-link target="_blank" :to="{ path: '/read', query: { id: row.id } }" v-if="row.status === 2">
+                <el-button type="success" size="mini" round>查看试卷</el-button>
+              </router-link>
+            </template>
+          </el-table-column>
+        </el-table>
+        <pagination v-show="total > 0" :total="total" :background="true" :page.sync="queryParam.pageIndex"
+          :limit.sync="queryParam.pageSize" @pagination="search" style="margin-top: 24px" />
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -63,7 +78,7 @@ import examPaperAnswerApi from '@/api/examPaperAnswer'
 import { scrollTo } from '@/utils/scroll-to'
 export default {
   components: { Pagination },
-  data () {
+  data() {
     return {
       queryParam: {
         pageIndex: 1,
@@ -82,12 +97,12 @@ export default {
       }
     }
   },
-  created () {
+  created() {
     this.search()
     scrollTo(0, 800)
   },
   methods: {
-    search () {
+    search() {
       this.listLoading = true
       let _this = this
       examPaperAnswerApi.pageList(this.queryParam).then(data => {
@@ -98,13 +113,13 @@ export default {
         _this.listLoading = false
       })
     },
-    itemSelect (row, column, event) {
+    itemSelect(row, column, event) {
       this.selectItem = row
     },
-    statusTagFormatter (status) {
+    statusTagFormatter(status) {
       return this.enumFormat(this.statusTag, status)
     },
-    statusTextFormatter (status) {
+    statusTextFormatter(status) {
       return this.enumFormat(this.statusEnum, status)
     }
   },
@@ -121,5 +136,134 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+// 纵向排列容器
+.modern-record-page {
+  min-height: 100vh;
+  background: #f6f8fa;
+  padding: 32px 0 32px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
+.modern-column-layout {
+  width: 100%;
+  max-width: 900px;
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+}
+
+.modern-table-card {
+  border-radius: 18px;
+  box-shadow: 0 4px 24px 0 rgba(0, 0, 0, 0.07);
+  padding: 0 0 24px 0;
+  background: #fff;
+  min-height: 420px;
+}
+
+.modern-table-title {
+  display: flex;
+  align-items: center;
+  font-size: 1.25rem;
+  font-weight: 600;
+  color: #222;
+  padding: 18px 0 0 0;
+}
+
+.modern-avatar {
+  margin-right: 12px;
+  background: linear-gradient(135deg, #6dd5ed 0%, #2193b0 100%);
+  color: #fff;
+}
+
+.modern-table {
+  border-radius: 12px;
+  overflow: hidden;
+  font-size: 15px;
+}
+
+.modern-table ::v-deep .el-table__header th {
+  background: #f0f4f8;
+  font-weight: bold;
+  color: #333;
+}
+
+.modern-table ::v-deep .el-table__row:hover {
+  background: #e6f7ff;
+}
+
+.modern-info-card {
+  border-radius: 18px;
+  box-shadow: 0 4px 24px 0 rgba(0, 0, 0, 0.09);
+  background: #fff;
+  padding-bottom: 18px;
+  min-height: 180px;
+}
+
+.modern-info-header {
+  display: flex;
+  align-items: center;
+  padding: 18px 0 0 0;
+}
+
+.modern-info-avatar {
+  margin-right: 14px;
+  background: linear-gradient(135deg, #f7971e 0%, #ffd200 100%);
+  color: #fff;
+}
+
+.modern-info-title {
+  font-size: 1.1rem;
+  font-weight: 600;
+  color: #222;
+}
+
+.modern-info-list {
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.modern-info-item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 0;
+  border-bottom: 1px dashed #f0f0f0;
+  font-size: 15px;
+}
+
+.modern-info-item:last-child {
+  border-bottom: none;
+}
+
+.modern-info-label {
+  color: #888;
+}
+
+.modern-info-value {
+  font-weight: 600;
+  color: #222;
+  font-size: 1.1em;
+}
+
+.modern-info-value.highlight {
+  color: #ff6f00;
+  font-size: 1.3em;
+}
+
+@media (max-width: 991px) {
+  .modern-column-layout {
+    max-width: 100vw;
+    padding: 0 6px;
+    gap: 16px;
+  }
+  .modern-table-card,
+  .modern-info-card {
+    min-width: 0;
+    width: 100%;
+  }
+}
 </style>
