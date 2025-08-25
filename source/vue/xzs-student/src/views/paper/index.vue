2b2b96761af2b8593be1a2c28b855e46f02a217d
SakurakoujiHakuya <53645627+SakurakoujiHakuya@users.noreply.github.com>
2025-08-25 21:53:54 +0800

    试卷中心样式

--- source/vue/xzs-student/src/views/paper/index.vue
+++ source/vue/xzs-student/src/views/paper/index.vue
@@ -1,28 +1,33 @@
 <template>
-  <div style="margin-top: 10px" class="app-contain">
-    <el-tabs tab-position="left"  v-model="tabId"  @tab-click="subjectChange" >
-      <el-tab-pane :label="item.name"  :key="item.id" :name="item.id" v-for="item in subjectList" style="margin-left: 20px;" >
-        <el-row  style="float: right">
-          <el-radio-group v-model="queryParam.paperType" size="mini" @change="paperTypeChange" >
-            <el-radio v-for="item in paperTypeEnum" size="mini" :key="item.key" :label="item.key">{{item.value}}</el-radio>
-          </el-radio-group>
-        </el-row>
-        <el-table v-loading="listLoading" :data="tableData" fit highlight-current-row style="width: 100%">
-          <el-table-column prop="id" label="序号" width="90px"/>
-          <el-table-column prop="name" label="名称"  />
-          <el-table-column align="right">
-            <template slot-scope="{row}">
-              <router-link target="_blank" :to="{path:'/do',query:{id:row.id}}">
-                <el-button  type="text" size="small">开始答题</el-button>
-              </router-link>
-            </template>
-          </el-table-column>
-        </el-table>
-        <pagination v-show="total>0" :total="total" :background="false" :page.sync="queryParam.pageIndex" :limit.sync="queryParam.pageSize"
-                    @pagination="search" style="margin-top: 20px"/>
-      </el-tab-pane>
-    </el-tabs>
-
+  <div class="paper-container">
+    <el-card class="paper-card" shadow="never">
+      <el-tabs v-model="tabId" @tab-click="subjectChange" class="paper-tabs">
+        <el-tab-pane :label="item.name" :key="item.id" :name="item.id" v-for="item in subjectList">
+          <div class="filter-bar">
+            <el-radio-group v-model="queryParam.paperType" size="small" @change="paperTypeChange">
+              <el-radio-button v-for="item in paperTypeEnum" :key="item.key"
+                :label="item.key">{{ item.value }}</el-radio-button>
+            </el-radio-group>
+          </div>
+          <el-table v-loading="listLoading" :data="tableData" fit highlight-current-row class="paper-table" stripe>
+            <el-table-column prop="id" label="序号" width="90" align="center" />
+            <el-table-column prop="name" label="试卷名称" min-width="200" />
+            <el-table-column prop="questionCount" label="题目数" width="100" align="center" />
+            <el-table-column prop="score" label="总分" width="100" align="center" />
+            <el-table-column prop="createTime" label="创建时间" width="180" align="center" />
+            <el-table-column label="操作" width="120" align="center">
+              <template slot-scope="{row}">
+                <router-link target="_blank" :to="{ path: '/do', query: { id: row.id } }">
+                  <el-button type="primary" size="mini" round>开始答题</el-button>
+                </router-link>
+              </template>
+            </el-table-column>
+          </el-table>
+          <pagination v-show="total > 0" :total="total" :page.sync="queryParam.pageIndex"
+            :limit.sync="queryParam.pageSize" @pagination="search" />
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
   </div>
 </template>
 
@@ -34,7 +39,7 @@ import subjectApi from '@/api/subject'
 
 export default {
   components: { Pagination },
-  data () {
+  data() {
     return {
       queryParam: {
         paperType: 1,
@@ -49,11 +54,11 @@ export default {
       total: 0
     }
   },
-  created () {
+  created() {
     this.initSubject()
   },
   methods: {
-    initSubject () {
+    initSubject() {
       let _this = this
       subjectApi.list().then(re => {
         _this.subjectList = re.response
@@ -63,7 +68,7 @@ export default {
         _this.search()
       })
     },
-    search () {
+    search() {
       this.listLoading = true
       examPaperApi.pageList(this.queryParam).then(data => {
         const re = data.response
@@ -73,10 +78,10 @@ export default {
         this.listLoading = false
       })
     },
-    paperTypeChange (val) {
+    paperTypeChange(val) {
       this.search()
     },
-    subjectChange (tab, event) {
+    subjectChange(tab, event) {
       this.queryParam.subjectId = Number(this.tabId)
       this.search()
     }
