5d0544de7a51446a72fcb8b096e8d479095bbba1
SakurakoujiHakuya <53645627+SakurakoujiHakuya@users.noreply.github.com>
2025-08-25 22:28:57 +0800

    错题本修改

--- source/vue/xzs-student/src/api/questionAnswer.js
+++ source/vue/xzs-student/src/api/questionAnswer.js
@@ -1,6 +1,78 @@
 import { post } from '@/utils/request'
 
 export default {
-  pageList: query => post('/api/student/question/answer/page', query),
-  select: id => post('/api/student/question/answer/select/' + id)
+  pageList: query => Promise.resolve({
+    "code": 1,
+    "message": "成功",
+    "response": {
+        "total": 4,
+        "list": [
+            {
+                "id": 24928,
+                "questionType": 1,
+                "createTime": "2021-06-02 16:07:11",
+                "subjectName": "语文",
+                "shortTitle": "下列哪个成语是形容说话不算数的？"
+            },
+            {
+                "id": 24929,
+                "questionType": 2,
+                "createTime": "2021-06-03 11:20:45",
+                "subjectName": "数学",
+                "shortTitle": "以下哪些是偶数？"
+            },
+            {
+                "id": 24930,
+                "questionType": 3,
+                "createTime": "2021-06-04 09:05:10",
+                "subjectName": "英语",
+                "shortTitle": "The sun rises in the west."
+            },
+            {
+                "id": 24931,
+                "questionType": 4,
+                "createTime": "2021-06-05 14:30:00",
+                "subjectName": "物理",
+                "shortTitle": "万有引力是谁发现的？"
+            }
+        ]
+    }
+}),
+  select: id => Promise.resolve(
+    {
+      "code": 1,
+      "message": "成功",
+      "response": {
+          "questionVM": {
+              "id": 507,    //题目id
+              "questionType": 1,   //题目类型
+              "subjectId": 46,  //学科id
+              "title": "下列哪个成语是形容说话不算数的？",   //题干
+              "gradeLevel": 12,    //年级
+              "items": [        //选项
+                {
+                    "prefix": "A",  //选项
+                    "content": "A",  //选项内容
+                    "score": null    //选项分数
+                }
+            ],
+            "analyze": "D",     //解析
+            "correctArray": null,  //标答
+            "correct": "D",   //标答
+            "score": "2",  //分数
+            "difficult": 3,  //难度
+            "itemOrder": null  //排序
+        },
+        "questionAnswerVM": {   //用户答案
+            "id": 25067,  
+            "questionId": 507,  //题目id
+            "doRight": false,   //是否正确
+            "content": "A",   //用户答案
+            "itemOrder": 2,   //排序
+            "contentArray": null,   //用户答案
+            "score": "0",  //得分
+            "questionScore": "2"  //题目分数
+        }
+    }
+})
 }
--- source/vue/xzs-student/src/views/question-error/index.vue
+++ source/vue/xzs-student/src/views/question-error/index.vue
@@ -1,26 +1,37 @@
 <template>
-  <div style="margin-top: 10px" class="app-contain">
-    <el-row :gutter="50">
-      <el-col :span="14">
-        <el-table v-loading="listLoading" :data="tableData" fit highlight-current-row style="width: 100%" @row-click="itemSelect">
-          <el-table-column prop="shortTitle" label="题干"  show-overflow-tooltip />
-          <el-table-column prop="questionType" label="题型"  :formatter="questionTypeFormatter" width="70" />
-          <el-table-column prop="subjectName" label="学科"  width="50" />
-          <el-table-column prop="createTime" label="做题时间"  width="170" />
-        </el-table>
-        <pagination v-show="total>0" :total="total" :background="false" :page.sync="queryParam.pageIndex" :limit.sync="queryParam.pageSize"
-                    @pagination="search" style="margin-top: 20px"/>
-      </el-col>
-      <el-col  :span="10" >
-        <el-card  class="record-answer-info">
-          <el-form>
-            <el-form-item>
-              <QuestionAnswerShow :qType="selectItem.questionType" :qLoading="qAnswerLoading" :question="selectItem.questionItem"  :answer="selectItem.answerItem"/>
-            </el-form-item>
-          </el-form>
-        </el-card>
-      </el-col>
-    </el-row>
+  <div class="question-error-container">
+    <el-card class="box-card" shadow="never">
+      <div slot="header" class="card-header">
+        <span>我的错题本</span>
+      </div>
+      <el-row :gutter="30">
+        <el-col :span="14">
+          <el-table v-loading="listLoading" :data="tableData" fit highlight-current-row class="question-table"
+            @row-click="itemSelect" stripe>
+            <el-table-column prop="shortTitle" label="题干" show-overflow-tooltip min-width="200" />
+            <el-table-column prop="questionType" label="题型" width="100" align="center">
+              <template slot-scope="{ row }">
+                <el-tag size="small">{{ questionTypeFormatter(row, null, row.questionType) }}</el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column prop="subjectName" label="学科" width="100" align="center" />
+            <el-table-column prop="createTime" label="做题时间" width="180" align="center" />
+          </el-table>
+          <pagination v-show="total > 0" :total="total" :background="true" :page.sync="queryParam.pageIndex"
+            :limit.sync="queryParam.pageSize" @pagination="search" />
+        </el-col>
+        <el-col :span="10">
+          <el-card class="question-detail-card" shadow="never">
+            <div slot="header" class="card-header">
+              <span>题目详情与解析</span>
+            </div>
+            <el-skeleton :rows="10" animated v-if="qAnswerLoading" />
+            <QuestionAnswerShow v-else :qType="selectItem.questionType" :question="selectItem.questionItem"
+              :answer="selectItem.answerItem" />
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
@@ -32,7 +43,7 @@ import QuestionAnswerShow from '../exam/components/QuestionAnswerShow'
 
 export default {
   components: { Pagination, QuestionAnswerShow },
-  data () {
+  data() {
     return {
       queryParam: {
         pageIndex: 1,
@@ -49,11 +60,11 @@ export default {
       }
     }
   },
-  created () {
+  created() {
     this.search()
   },
   methods: {
-    search () {
+    search() {
       this.listLoading = true
       let _this = this
       questionAnswerApi.pageList(this.queryParam).then(data => {
@@ -67,10 +78,10 @@ export default {
         }
       })
     },
-    itemSelect (row, column, event) {
+    itemSelect(row, column, event) {
       this.qAnswerShow(row.id)
     },
-    qAnswerShow (id) {
+    qAnswerShow(id) {
       let _this = this
       this.qAnswerLoading = true
       questionAnswerApi.select(id).then(re => {
@@ -81,7 +92,7 @@ export default {
         _this.qAnswerLoading = false
       })
     },
-    questionTypeFormatter (row, column, cellValue, index) {
+    questionTypeFormatter(row, column, cellValue, index) {
       return this.enumFormat(this.questionTypeEnum, cellValue)
     }
   },
@@ -95,5 +106,48 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.question-error-container {
+  padding: 24px;
+  background-color: #f5f7fa;
+  min-height: calc(100vh - 84px);
+}
 
+.box-card {
+  border-radius: 12px;
+  border: none;
+}
+
+.card-header {
+  font-size: 18px;
+  font-weight: 600;
+  color: #303133;
+}
+
+.question-table {
+  border-radius: 8px;
+  overflow: hidden;
+
+  &::v-deep .el-table__header th {
+    background: #fafafa;
+    font-weight: 600;
+    color: #333;
+  }
+
+  &::v-deep .el-table__row:hover {
+    background: #f5f7fa;
+  }
+}
+
+.question-detail-card {
+  border-radius: 12px;
+  min-height: 500px;
+
+  &::v-deep .el-card__header {
+    border-bottom: 1px solid #ebeef5;
+  }
+
+  &::v-deep .el-card__body {
+    padding: 20px;
+  }
+}
 </style>
